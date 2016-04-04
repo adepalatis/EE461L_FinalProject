fcbeb03375bcc72dfce92efb6b7d173b9a9cb41f
Anthony DePalatis <111tdepalatis@gmail.com>
2016-04-04 13:49:17 -0500

    Added transitions between activities. Initialized view for IngredientEntryActivity.

--- RecipEZ/app/src/main/java/com/github/adepalatis/ee461/recipez/IngredientEntryActivity.java
+++ RecipEZ/app/src/main/java/com/github/adepalatis/ee461/recipez/IngredientEntryActivity.java
@@ -4,28 +4,48 @@ package com.github.adepalatis.ee461.recipez;
  * Created by Tony on 3/27/2016.
  */
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.GridView;
 import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 
-public class IngredientEntryActivity extends AppCompatActivity {
+public class IngredientEntryActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Spinner cuisineSpinner = (Spinner)findViewById(R.id.cuisine_spinner);
-    private GridView selectedIngredients = (GridView) findViewById(R.id.gridView);
+    private Spinner cuisineSpinner;
+    private GridView selectedIngredients;
+    private Button findRecipesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingredient_entry);
+
+        // Configure Button listener
+        try {
+            findViewById(R.id.findRecipesButton).setOnClickListener(this);
+        } catch(Exception e) {
+            System.err.println(e.toString());
+        }
+
+        // Initialize spinner, list of selected ingredients, ingredient entry field, checkboxes, and button
+        cuisineSpinner = (Spinner)findViewById(R.id.cuisine_spinner);
+        selectedIngredients = (GridView)findViewById(R.id.gridView);
+        findRecipesButton = (Button)findViewById(R.id.findRecipesButton);
 
         // Configure the cuisine spinner
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.cuisine_array, android.R.layout.simple_spinner_dropdown_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         cuisineSpinner.setAdapter(adapter);
+    }
 
-        // Configure the list of selected ingredients
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this, ShowRecipesActivity.class));
     }
 
 }
--- RecipEZ/app/src/main/java/com/github/adepalatis/ee461/recipez/ShowRecipesActivity.java
+++ RecipEZ/app/src/main/java/com/github/adepalatis/ee461/recipez/ShowRecipesActivity.java
@@ -2,15 +2,22 @@ package com.github.adepalatis.ee461.recipez;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ExpandableListView;
 
 /**
  * Created by Tony on 3/27/2016.
  */
 public class ShowRecipesActivity extends AppCompatActivity {
 
+    private ExpandableListView recipes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_recipes);
+
+        // Initialize list of search results
+        recipes = (ExpandableListView)findViewById(R.id.expandableListView);
     }
 
 }
