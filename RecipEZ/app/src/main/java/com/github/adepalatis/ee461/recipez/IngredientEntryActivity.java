eebbdae2562b78572ad2d07a45527441864ea572
Anthony DePalatis <111tdepalatis@gmail.com>
2016-04-04 15:43:57 -0500

    Added a button to add an ingredient to the list, from what the user types (currently faulty)

--- RecipEZ/app/src/main/java/com/github/adepalatis/ee461/recipez/IngredientEntryActivity.java
+++ RecipEZ/app/src/main/java/com/github/adepalatis/ee461/recipez/IngredientEntryActivity.java
@@ -8,34 +8,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.ListAdapter;
 import android.widget.SimpleAdapter;
 import android.widget.Spinner;
 
-public class IngredientEntryActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+
+public class IngredientEntryActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemSelectedListener {
 
     private Spinner cuisineSpinner;
+    private EditText ingredientEntryBox;
     private GridView selectedIngredients;
     private Button findRecipesButton;
+    private Button addIngredientButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ingredient_entry);
 
-        // Configure Button listener
+        // Initialize spinner, list of selected ingredients, ingredient entry field, checkboxes, and button
+        cuisineSpinner = (Spinner)findViewById(R.id.cuisine_spinner);
+        ingredientEntryBox = (EditText)findViewById(R.id.ingredientEntryBox);
+        selectedIngredients = (GridView)findViewById(R.id.gridView);
+        findRecipesButton = (Button)findViewById(R.id.findRecipesButton);
+        addIngredientButton = (Button)findViewById(R.id.addIngredientButton);
+
+        // Configure listeners
         try {
-            findViewById(R.id.findRecipesButton).setOnClickListener(this);
+            findRecipesButton.setOnClickListener(this);
+            addIngredientButton.setOnClickListener(this);
+            cuisineSpinner.setOnItemSelectedListener(this);
         } catch(Exception e) {
             System.err.println(e.toString());
         }
-
-        // Initialize spinner, list of selected ingredients, ingredient entry field, checkboxes, and button
-        cuisineSpinner = (Spinner)findViewById(R.id.cuisine_spinner);
-        selectedIngredients = (GridView)findViewById(R.id.gridView);
-        findRecipesButton = (Button)findViewById(R.id.findRecipesButton);
 
         // Configure the cuisine spinner
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.cuisine_array, android.R.layout.simple_spinner_dropdown_item);
@@ -44,8 +55,35 @@ public class IngredientEntryActivity extends AppCompatActivity implements View.O
     }
 
     @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        CharSequence selectedCuisine = (CharSequence)parent.getItemAtPosition(pos);
+        if(selectedCuisine.toString().equals("Chinese")) {
+            ((Button) findViewById(R.id.findRecipesButton)).setText(R.string.test_string);
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
     public void onClick(View v) {
-        startActivity(new Intent(this, ShowRecipesActivity.class));
+        switch(v.getId()) {
+            case R.id.findRecipesButton:
+                startActivity(new Intent(this, ShowRecipesActivity.class));
+                break;
+
+            case R.id.addIngredientButton:
+                addIngredientToGrid(ingredientEntryBox.getText());
+                break;
+        }
+    }
+
+    public void addIngredientToGrid(CharSequence ingredient) {
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this, R.layout.activity_show_recipes);
+        adapter.add(ingredient);
+        selectedIngredients.setAdapter(adapter);
     }
 
 }
