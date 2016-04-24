74e23778ff150a81a093a91b8aec747b4f1153b6
Michael Wang <ziwang1111@gmail.com>
2016-04-24 17:50:04 -0500

    get data in showrecipeactivity

--- RecipEZ/app/src/main/java/com/github/adepalatis/ee461/recipez/Ingredient.java
+++ RecipEZ/app/src/main/java/com/github/adepalatis/ee461/recipez/Ingredient.java
@@ -1,20 +1,74 @@
 package com.github.adepalatis.ee461.recipez;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by michael on 4/20/16.
  */
-public class Ingredient {
-    public String aisle = null;
-    public String name = null;
-    public Double amount = null;
-    public String unit = null;
-    public String originalString = null;
-    public String image = null;
+public class Ingredient implements Parcelable {
+    private String aisle = null;
+    private String name = null;
+    private String unit = null;
+    private String originalString = null;
+    private String image = null;
+    private Double amount = null;
 
-    public Ingredient() {}
+    public String getAisle() {
+        return aisle;
+    }
+
+    public void setAisle(String aisle) {
+        this.aisle = aisle;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getOriginalString() {
+        return originalString;
+    }
+
+    public void setOriginalString(String originalString) {
+        this.originalString = originalString;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Double amount) {
+        this.amount = amount;
+    }
 
     public Ingredient(String name) {
         this.name = name;
+    }
+
+    public Ingredient(Parcel p) {
+        aisle = p.readString();
+        name = p.readString();
+        unit = p.readString();
+        originalString = p.readString();
+        image = p.readString();
+        amount = p.readDouble();
     }
 
     public String getName() {
@@ -54,4 +108,31 @@ public class Ingredient {
 
         return s + "}";
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(aisle);
+        dest.writeString(name);
+        dest.writeString(unit);
+        dest.writeString(originalString);
+        dest.writeString(image);
+        dest.writeDouble(amount);
+    }
+
+    public static final Parcelable.Creator<Ingredient> CREATOR = new Parcelable.Creator<Ingredient>() {
+        @Override
+        public Ingredient createFromParcel(Parcel source) {
+            return new Ingredient(source);
+        }
+
+        @Override
+        public Ingredient[] newArray(int size) {
+            return new Ingredient[size];
+        }
+    };
 }
--- RecipEZ/app/src/main/java/com/github/adepalatis/ee461/recipez/ShowRecipesActivity.java
+++ RecipEZ/app/src/main/java/com/github/adepalatis/ee461/recipez/ShowRecipesActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ExpandableListView;
 
+import java.util.List;
+
 /**
  * Created by Tony on 3/27/2016.
  */
@@ -16,7 +18,26 @@ public class ShowRecipesActivity extends AppCompatActivity {
 
         Bundle data = getIntent().getExtras();
         if (data != null) {
-            
+            RecipeSearchParameters rsp = data.getParcelable("RSP");
+            List<RecipeSearchResult> rsr;
+
+            FoodAPI api = FoodAPI.getInstance();
+            if (rsp.getQuery() != null) { // Use query recipe search
+                try {
+                    rsr = api.searchRecipes(rsp.getCuisine(), rsp.getDiet(),
+                            rsp.getExcludeIngredients(), rsp.getIntolerance(), rsp.isLimitLicense(),
+                            rsp.getMaxNumber(), rsp.getOffset(), rsp.getQuery(), rsp.getType());
+                } catch(Exception e) {
+                    System.err.println(e);
+                }
+            } else {
+                try {
+                    rsr = api.searchRecipes(rsp.isIngredientLists(), rsp.getIngredients(),
+                            rsp.isLimitLicense(), rsp.getMaxNumber(), rsp.getRanking());
+                } catch(Exception e) {
+                    System.err.println(e);
+                }
+            }
         }
     }
 
