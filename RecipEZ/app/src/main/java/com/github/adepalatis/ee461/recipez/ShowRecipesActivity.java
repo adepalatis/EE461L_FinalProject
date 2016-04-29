ca0dd870c9eb8c2c2b8814440e98e80d73f1a4a1
Michael Wang <ziwang1111@gmail.com>
2016-04-29 18:15:28 -0500

    update image for adapter

--- RecipEZ/app/src/main/java/com/github/adepalatis/ee461/recipez/ShowRecipesActivity.java
+++ RecipEZ/app/src/main/java/com/github/adepalatis/ee461/recipez/ShowRecipesActivity.java
@@ -28,7 +28,7 @@ public class ShowRecipesActivity extends AppCompatActivity {
         Bundle data = getIntent().getExtras();
 //        if (data != null) {
             RecipeSearchParameters rsp = data.getParcelable("RSP");
-            List<RecipeSearchResult> rsr = null;
+            List<RecipeSearchResult> rsr;
 
             FoodAPI api = FoodAPI.getInstance();
             if (!rsp.getQuery().equals("")) { // Use query recipe search
